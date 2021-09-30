b8bd2131d6257052b5d7863c7286afec4182f830
victormerch <1Victormerch@gmail.com>
2021-09-30 17:04:45 +0200

    subida de la pre-alpha

--- app/src/main/java/com/example/endivinanumero/MainActivity.java
+++ app/src/main/java/com/example/endivinanumero/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.endivinanumero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        int numero = (int)(Math.random()*100+1);
+
+        final EditText numeroEditText = (EditText) findViewById(R.id.editTextNumber);
+        final Button button = findViewById(R.id.button);
+        final TextView aviso = (TextView) findViewById(R.id.aviso);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                //Cogemos el numero de el EditTextNumber
+
+                int numero2 = Integer.parseInt(numeroEditText.getText().toString());
+
+                System.out.println("-Numero random: "+numero+"\n-Numero ecogido: "+numero2);
+                //Comprobamos si coincide con el numero random de entre el 1 al 100
+                if (numero==numero2){
+                    
+                    aviso.setTextColor(Color.GREEN);
+                    aviso.setText("Numero Correcto!");
+                }else{
+
+                    aviso.setTextColor(Color.RED);
+                    aviso.setText("Numero Incorrecto!");
+                }
+            }
+        });
+
+
     }
+
+
+
+
 }
