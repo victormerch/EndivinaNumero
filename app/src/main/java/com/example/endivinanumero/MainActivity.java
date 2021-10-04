866e156481a4dad2ee1e3d6f8be2b1061f45342a
victormerch <1Victormerch@gmail.com>
2021-10-04 16:21:41 +0200

    commit de tercera session

--- app/src/main/java/com/example/endivinanumero/MainActivity.java
+++ app/src/main/java/com/example/endivinanumero/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.endivinanumero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private int intentos;
@@ -31,25 +33,41 @@ public class MainActivity extends AppCompatActivity {
 
         textIntent.setText(String.valueOf(intentos));
         button.setOnClickListener(new View.OnClickListener() {
-
+            //Si clica y no ha rellenado nada hay que hacer una condicion para que no pete
             public void onClick(View v) {
-                //Cogemos el numero de el EditTextNumber
 
-                int numero2 = Integer.parseInt(numeroEditText.getText().toString());
+                try{
 
-                System.out.println("-Numero random: "+numero+"\n-Numero ecogido: "+numero2);
-                //Comprobamos si coincide con el numero random de entre el 1 al 100
-                if (numero==numero2){
+                    //Cogemos el numero de el EditTextNumber
+                    int numero2 = Integer.parseInt(numeroEditText.getText().toString());
+                    numeroEditText.getText().clear();
 
-                    aviso.setTextColor(Color.GREEN);
-                    aviso.setText("Numero Correcto!");
-                }else{
-                    intentos++;
-                    textIntent.setText(String.valueOf(intentos));
-                    aviso.setTextColor(Color.RED);
-                    aviso.setText("Numero Incorrecto!");
+                    System.out.println("-Numero random: "+numero+"\n-Numero ecogido: "+numero2);
+                    //Comprobamos si coincide con el numero random de entre el 1 al 100
+                    if (numero==numero2){
+
+                        aviso.setTextColor(Color.GREEN);
+                        aviso.setText("Numero Correcto!");
+                    }else{
+                        intentos++;
+                        textIntent.setText(String.valueOf(intentos));
+                        aviso.setTextColor(Color.RED);
+                        if (numero2>numero){
+                            aviso.setText(numero2+" es mas grande que el numero escogido");
+                        }else{
+                            aviso.setText(numero2+" es mas pequeño que el numero escogido");
+                        }
+
+                    }
+                }catch(Exception E){
+
+                    Context context = getApplicationContext();
+
+                    Toast.makeText(context,"Tienes que introducir un numero",Toast.LENGTH_SHORT).show();
                 }
+
             }
+
         });
 
 
