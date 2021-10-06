f5d35ebad8831be3205b41ee2a8fd60b93830b83
victormerch <1Victormerch@gmail.com>
2021-10-06 20:16:54 +0200

    añadim el dialog

--- app/src/main/java/com/example/endivinanumero/MainActivity.java
+++ app/src/main/java/com/example/endivinanumero/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.endivinanumero;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -109,11 +115,37 @@ public class MainActivity extends AppCompatActivity {
                         aviso.setTextColor(Color.GREEN);
                         aviso.setText("Numero Correcto!");
                         intentos = 0;
+
+                        AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+
+                        alert.setTitle("Input Name");
+                        alert.setMessage("Introduce tu nombre para que se vea en el ranking:");
+
+                        // Set an EditText view to get user input
+                        final EditText input = new EditText(MainActivity.this);
+                        alert.setView(input);
+
+                        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                Editable value = input.getText();
+                                // Do something with value!
+                            }
+                        });
+
+                        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // Canceled.
+                            }
+                        });
+
+                        alert.show();
+
+
                     }else{
                         intentos++;
                         textIntent.setText(String.valueOf(intentos));
                         aviso.setTextColor(Color.RED);
-                        if (numero2>numero){
+                        if (numero2<numero){
                             aviso.setText("El numero es mas grande que "+ numero2);
                         }else{
                             aviso.setText("El numero es mas pequeño que "+ numero2);
@@ -138,3 +170,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
